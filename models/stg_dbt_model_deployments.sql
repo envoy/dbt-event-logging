739b50645a4f1fa6c62afdebfd493aef49a0ccd4
David Wallace <dwallace0723@gmail.com>
2019-02-28 16:12:57 -0800

    add in event schema

--- models/stg_dbt_model_deployments.sql
+++ models/stg_dbt_model_deployments.sql
@@ -7,6 +7,7 @@ aggregated as (
 select
   {{dbt_utils.surrogate_key('event_model', 'invocation_id')}} as model_deployment_id,
   invocation_id,
+  event_schema as schema,
   event_model as model,
   is_full_refresh,
   min(case when event_name = 'model deployment started' then event_timestamp end) as deployment_started_at,
@@ -14,7 +15,7 @@ select
   datediff('seconds',deployment_started_at,deployment_completed_at) as deployment_time_seconds
 from events
 where event_name ilike '%model%'
-group by 1, 2, 3, 4
+group by 1, 2, 3, 4, 5
 )
 
 select *
